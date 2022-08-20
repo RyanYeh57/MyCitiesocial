04c657ccb6af3e94f1e9b67da0ea75fc4e0ee9e8
Ryan Yeh <ej68433221ik@gmail.com>
2022-08-20 18:28:13 +0800

    devise configuring views

--- app/frontend/scripts/shared/application.js
+++ app/frontend/scripts/shared/application.js
@@ -16,3 +16,14 @@ document.addEventListener('turbokinks:load', () => {
   });
 
 });
+
+// notification
+document.addEventListener('turbolinks:load', () => {
+  (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+    const $notification = $delete.parentNode;
+
+    $delete.addEventListener('click', () => {
+      $notification.parentNode.removeChild($notification);
+    });
+  });
+});
