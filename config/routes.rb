559b6c91e9aa4d2c56cb9424a4e9ed686e018f16
Ryan Yeh <ej68433221ik@gmail.com>
2022-08-20 21:19:06 +0800

    backend setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'products#index'
 
   resources :products, only:[:index, :show]
+
+  namespace :admin do
+    root 'products#index'
+    resources :products
+  end
 
 end
